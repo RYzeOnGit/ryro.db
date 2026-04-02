9c83c5bb575a9d698e53c52c6a4f084cfd8a7394
Ryan <ryan.chattopadhyay1@example.com>
2026-04-02 15:12:41 -0700

    Ownership

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -8,5 +8,14 @@ fn main() {
     y = 20;
     println!("y after = {}", y);
 
-    
+    let a = "hello";                // &str
+    let b = String::from("hello");  // String
+
+    println!("a = {}", a);
+    println!("b = {}", b);
+
+    let key = "name";
+    let key2 = key.to_string();
+    println!("{}", key2);
+
 }
