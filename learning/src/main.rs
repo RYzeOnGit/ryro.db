5f8e4d778b82188d4d331cd6b2022edab78581cf
Ryan <ryan.chattopadhyay1@example.com>
2026-04-02 15:10:12 -0700

    variables in rust

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let mut y = 10;
+
+
+    println!("{}", x);
+    println!("y before = {}", y);
+    y = 20;
+    println!("y after = {}", y);
+
+    
 }
